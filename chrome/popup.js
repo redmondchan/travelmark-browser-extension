c526ae244ea9a9d80dee5b9b5bb0a657fe322ee0
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-11 15:37:34 -0400

    added js to disable and enable city list input and commented out js for custom search dropdown

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,34 +1,42 @@
-window.onload = function () {
-    document.getElementById("myInput").addEventListener("click", myFunction);
-
-    let dropdownOptions = document.getElementsByClassName("dropdown-option")
-    for(let i = 0; i < dropdownOptions.length; i++) {
-        dropdownOptions[i].addEventListener("click", (e) => {
-          console.log(e.currentTarget.textContent)
-          document.getElementById("myInput").value = e.currentTarget.textContent
-          document.getElementById("myDropdown").classList.toggle("show");
-        })
-    }
-    console.log("add")
-}
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-  console.log("testttttttt")
-}
-
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
+window.onload = function() {
+  // document.getElementById("myInput").addEventListener("click", myFunction);
+  //
+  // let dropdownOptions = document.getElementsByClassName("dropdown-option")
+  // for(let i = 0; i < dropdownOptions.length; i++) {
+  //     dropdownOptions[i].addEventListener("click", (e) => {
+  //       console.log(e.currentTarget.textContent)
+  //       document.getElementById("myInput").value = e.currentTarget.textContent
+  //       document.getElementById("myDropdown").classList.toggle("show");
+  //     })
+  // }
+  // console.log("add")
+  console.log("test")
+  document.getElementById("input__country").addEventListener('change', (event) => {
+    if (event.target.value != "") {
+      document.getElementById("input__city").removeAttribute("disabled")
     } else {
-      a[i].style.display = "none";
+      document.getElementById("input__city").setAttribute("disabled", true)
     }
-  }
+  })
 }
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+//   console.log("testttttttt")
+// }
+//
+// function filterFunction() {
+//   var input, filter, ul, li, a, i;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   div = document.getElementById("myDropdown");
+//   a = div.getElementsByTagName("a");
+//   for (i = 0; i < a.length; i++) {
+//     txtValue = a[i].textContent || a[i].innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       a[i].style.display = "";
+//     } else {
+//       a[i].style.display = "none";
+//     }
+//   }
+// }
