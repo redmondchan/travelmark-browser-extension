2968b98640d48c9cc9764c967bb82c9c1e1bf4cc
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-14 18:17:39 -0400

    added logic to clear selected city and cities in dropdown when country changes

--- chrome/popup.js
+++ chrome/popup.js
@@ -24,16 +24,29 @@ window.onload = function() {
       }
     });
 
+//removes all child nodes
+function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
 //creates city dropdown elements
   document.getElementById("input__country").addEventListener('change', (event) => {
+    //clears all city options so cities from other countries are not there when a user selects a different coutnry
+    let citiesDropdown = document.getElementById("cities")
+    removeAllChildNodes(cities)
+    //
+    let cityInput = document.getElementById("input__city")
+    cityInput.value = ""
+
     if (event.target.value != "") {
-      document.getElementById("input__city").removeAttribute("disabled")
+      cityInput.removeAttribute("disabled")
       let selectedCountryId = document.querySelector(`[value=${event.target.value}]`)
       fetch(`http://localhost:8083/findCitiesByCountryId/${selectedCountryId.id}`)
         .then(response => response.json())
         .then(cities => {
           for (let i = 0; i < cities.length; i++) {
-            let citiesDropdown = document.getElementById("cities")
+            //let citiesDropdown = document.getElementById("cities")
             let cityOption = document.createElement("option")
             cityOption.value = cities[i].name
             cityOption.id = cities[i].id
